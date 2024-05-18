b6bf51f1543ce46f13aebb8185b0fd8a52893cdd
LabbyMaple <144498823+LabbyMaple@users.noreply.github.com>
2024-05-17 18:01:01 -0700

    lol it's by b-day paw-ty tomorrow
    Yuka: Happy Birthday maya

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ $(function () {
 
 locationInput = document.getElementById("input-datalist")
 datalist = document.getElementById("list-city")
-
+console.log(datalist)
 
 datalist.innerHTML = "";
 
 temp = []
-
+var latitude = 0
+var longitude = 0
 
 document.addEventListener('DOMContentLoaded', e => {
     $('#input-datalist').autocomplete(({source:temp}))
@@ -25,6 +26,8 @@ function apiData(url){
     .catch(err => { throw err });
 }
 
+
+city_name = ""
 locationInput.addEventListener("input", async event =>{
     event.preventDefault()
     city_name =locationInput.value
@@ -44,8 +47,16 @@ locationInput.addEventListener("input", async event =>{
     }
 })
 
+async function onInput(){
+    url = `https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&limit=5&appid=f975d4b3a9c05c4d60c2354c925012ca`
+    data = await apiData(url)
+    latitude = data[0]["lat"]
+    longitude = data[0]["lon"]
+}
+
 locationInput.addEventListener("submit", event => {
     event.preventDefault()
+    
 })
 
 function addCity (cityName) {
@@ -55,12 +66,22 @@ function addCity (cityName) {
 }
 
 let weatherform = document.getElementById("weatherform")
-weatherform.addEventListener("submit" , event =>{
+weatherform.addEventListener("submit" ,async event =>{
     event.preventDefault()
     if(locationInput.value !=""){
-        document.location.href="searchresults.html"
+        // document.location.href="searchresults.html"
+        await getWeatherData()
     }
     else{
         alert("Enter city name")
     }
 })
+
+var weatherData = {}
+async function getWeatherData(){
+    console.log(latitude)
+    weatherurl = `https://archive-api.open-meteo.com/v1/archive?latitude=${latitude}&longitude=${longitude}&start_date=2024-05-01&end_date=2024-05-01&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&timezone=auto`
+    weatherData = await apiData(weatherurl)
+    console.log(`Elevation: ${weatherData["elevation"]}`)
+    console.log()
+}
