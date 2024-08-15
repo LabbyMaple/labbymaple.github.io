1904ae6eff5e5a444f88ffd8f091a09b45e3ce03
LabbyMaple <144498823+LabbyMaple@users.noreply.github.com>
2024-08-15 11:30:28 -0700

    s

--- cookieClicker/script.js
+++ cookieClicker/script.js
@@ -19,24 +19,24 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth()
-var button = document.getElementById("signup_button")
-button.addEventListener("click", async () => {
-  var email = document.getElementById("username")
-  var password = document.getElementById("password")
-  createUserWithEmailAndPassword(auth, email.value, password.value).then((userCredidential) => {
-    const user = userCredidential.user
-    window.location.href = "../cookieClicker"
-  })
-  .catch((error) => {
-    const errorCode = error.code
-    const errorMessage = error.message
-    alert(errorCode, errorMessage)
-  })
-  }
-)
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth()
+// var button = document.getElementById("signup_button")
+// button.addEventListener("click", async () => {
+//   var email = document.getElementById("username")
+//   var password = document.getElementById("password")
+//   createUserWithEmailAndPassword(auth, email.value, password.value).then((userCredidential) => {
+//     const user = userCredidential.user
+//     window.location.href = "../cookieClicker"
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code
+//     const errorMessage = error.message
+//     alert(errorCode, errorMessage)
+//   })
+//   }
+// )
 
 // var login_button = document.getElementById("login_button")
 // login_button.addEventListener("click", async () => {
