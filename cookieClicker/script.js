d038208be6f9286d05d512f0cbfb6067e1cb5005
LabbyMaple <144498823+LabbyMaple@users.noreply.github.com>
2024-08-15 11:26:32 -0700

    Update script.js

--- cookieClicker/script.js
+++ cookieClicker/script.js
@@ -38,18 +38,18 @@ button.addEventListener("click", async () => {
   }
 )
 
-var login_button = document.getElementById("login_button")
-login_button.addEventListener("click", async () => {
-  var email = document.getElementById("email")
-  var password = document.getElementById("password")
-  signInWithEmailAndPassword(auth, email.value, password.value).then((userCredidential) => {
-    const user = userCredidential.user
-    window.location.href = "../cookieClicker"
-  })
-  .catch((error) => {
-    const errorCode = error.code
-    const errorMessage = error.message
-    alert(errorCode, errorMessage)
-  })
-  }
-)
+// var login_button = document.getElementById("login_button")
+// login_button.addEventListener("click", async () => {
+//   var email = document.getElementById("email")
+//   var password = document.getElementById("password")
+//   signInWithEmailAndPassword(auth, email.value, password.value).then((userCredidential) => {
+//     const user = userCredidential.user
+//     window.location.href = "../cookieClicker"
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code
+//     const errorMessage = error.message
+//     alert(errorCode, errorMessage)
+//   })
+//   }
+// )
