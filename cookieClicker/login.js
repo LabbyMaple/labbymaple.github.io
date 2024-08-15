ec0298103e3838dcef5faf20b2e15dadc5603217
LabbyMaple <144498823+LabbyMaple@users.noreply.github.com>
2024-08-15 11:32:11 -0700

    d

--- cookieClicker/login.js
+++ cookieClicker/login.js
@@ -1,3 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkzHqoDZcJ0Y26kx6BMLMBUjdJgb3WsgA",
+  authDomain: "login-1e0bb.firebaseapp.com",
+  projectId: "login-1e0bb",
+  storageBucket: "login-1e0bb.appspot.com",
+  messagingSenderId: "642397245161",
+  appId: "1:642397245161:web:b65d789374a0b827ceea78",
+  measurementId: "G-9L70HGFYYN"
+};
+
 var login_button = document.getElementById("login_button")
 login_button.addEventListener("click", async () => {
   var email = document.getElementById("email")
