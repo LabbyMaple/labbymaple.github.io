4f931e7890aa494f0f046a65b98f9c5f0b6ad382
LabbyMaple <144498823+LabbyMaple@users.noreply.github.com>
2024-08-16 15:29:50 -0700

    y

--- cookieClicker/cookieclicker.js
+++ cookieClicker/cookieclicker.js
@@ -1,5 +1,11 @@
 cookie = document.getElementById("cookie")
+score = document.getElementById("score")
+
 cookie.addEventListener("hover", async () => {
     cookie.style.height = "450px"
     cookie.style.width = "450px"
+})
+
+cookie.addEventListener("click", async () => {
+    
 })
